bb9671fd0f3802af143789467bb21e8894b78e95
Mohsen <mohsenhashemi421@gmail.com>
2021-12-07 12:13:19 -0800

    view component folder added

--- DevCard_MVC/Models/Article.cs
+++ DevCard_MVC/Models/Article.cs
@@ -2,10 +2,27 @@
 {
     public class Article
     {
+        #region constructor
+
+        public Article(long id, string title, string description, string image, string client)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+            Client = client;
+        }
+
+        #endregion
+
+        #region properties
+
         public long Id { get; set; }
         public string Title { get; set; }
-        public string Client { get; set; }
-        public string Image { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
+        public string Client { get; set; }
+
+        #endregion
     }
 }
---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string name)
+        {
+            var projects = new List<Project>
+            {
+                new Project(1, "مدیریت خوابگاه", "مدیریت سیستم خوابگاه","project-1.jpg", "Ariyana"),
+                new Project(2, "اسنپ فود", "مدیریت سیستم ارسال غذا به کل کشور", "project-2.jpg", "People"),
+                new Project(3, "نوبت دهی آنلاین پزشکی", "مدیریت سیستم نوبت دهی", "project-3.jpg", "Doctors"),
+                new Project(4, "ثبتنام املاک", "سیستم ثبتنام آنلاین املاک", "project-4.jpg", "People")
+            };
+            return View("_Projects", projects);
+        }
+    }
+}
