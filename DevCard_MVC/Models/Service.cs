afec96551c69fbbed03efd3edd1f36cd3d8aa6f6
Mohsen <mohsenhashemi421@gmail.com>
2021-12-10 10:39:30 -0800

    service object added

---
+++ DevCard_MVC/Models/Service.cs
@@ -0,0 +1,14 @@
+﻿namespace DevCard_MVC.Models
+{
+    public class Service
+    {
+        public Service(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
