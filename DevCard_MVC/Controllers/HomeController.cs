a29bab9b6efd269073a130afd7bccc136620f7ec
Mohsen <mohsenhashemi421@gmail.com>
2021-12-10 10:38:16 -0800

    devcard contact section completed

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,19 +1,27 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
+        private readonly List<Service> _services = new List<Service>
         {
-        }
+            new Service(1, "نقره ای"),
+            new Service(2, "طلایی"),
+            new Service(3, "پلاتین"),
+            new Service(4, "الماسی")
+        };
 
         [HttpGet]
         public IActionResult Contact()
         {
-            var contact = new Contact();
+            var contact = new Contact
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
             return View(contact);
         }
 
@@ -25,10 +33,24 @@ namespace DevCard_MVC.Controllers
         //}
 
         [HttpPost]
-        public JsonResult Contact(Contact form)
+        public IActionResult Contact(Contact model)
         {
-            Console.WriteLine(form.ToString());
-            return Json(Ok());
+            model.Services = new SelectList(_services, "Id", "Name");
+
+            if (!ModelState.IsValid)
+            {
+                ViewData["error"] = "اطلاعات وارد شده صحیح نیست. لطفا مجددا تلاش کنید";
+                return View(model);
+            }
+
+            //return RedirectToAction("Index");
+            ModelState.Clear();
+            model.Email = "";
+            model.Name = "";
+            model.Message = "";
+            model.Service = 0;
+            ViewData["success"] = "پیغام شما با موفقیت ارسال شد. با تشکر";
+            return View(model);
         }
 
         public IActionResult Index()
